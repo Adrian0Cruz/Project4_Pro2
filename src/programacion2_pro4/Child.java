16c1ec3310818aac508584bdc834d8309ba7349c
Jesús Hernández <jesusalbertohd15@gmail.com>
2024-05-03 14:51:47 -0500

    i created sets, gets, empty constructor, method toString and avanced List class

--- src/programacion2_pro4/Child.java
+++ src/programacion2_pro4/Child.java
@@ -3,16 +3,39 @@
 package programacion2_pro4;
 //@author Jesús Hernández
 public class Child {
-    String Grade;
-    int Age;
-    int Id;
-    String Name;
-    String Gender;
+    private String Grade;
+    private int Age;
+    private int Id;
+    private String Name;
+    private String Gender;
     
     Child Sig;
 
-    public Child(String Grade, int Age, int Id, String Name, String Gender,
-            Child Sig) {
+    public Child () { }
+
+    public void setGrade ( String Grade ) { this.Grade = Grade; }
+
+    public void setAge ( int Age ) { this.Age = Age; }
+
+    public void setId ( int Id ) { this.Id = Id; }
+
+    public void setName ( String Name ) { this.Name = Name; }
+
+    public void setGender ( String Gender ) { this.Gender = Gender; }
+
+    
+    public String getGrade ( ) { return Grade; }
+
+    public int getAge ( ) { return Age; }
+
+    public int getId ( ) { return Id; }
+
+    public String getName ( ) { return Name; }
+
+    public String getGender ( ) { return Gender; }
+    
+
+    public Child ( String Grade, int Age, int Id, String Name, String Gender ) {
         this.Grade = Grade;
         this.Age = Age;
         this.Id = Id;
@@ -20,4 +43,14 @@ public class Child {
         this.Gender = Gender;
         this.Sig = null;
     }
+    
+
+    @Override
+    public String toString ( ) {
+        return  "Grado: " + Grade + 
+                "\nAge=" + Age + 
+                "\nId=" + Id + 
+                "\nName=" + Name + 
+                "\nGender=" + Gender + '}';
+    }   
 }
--- src/programacion2_pro4/List.java
+++ src/programacion2_pro4/List.java
@@ -3,5 +3,26 @@
 package programacion2_pro4;
 //@author Jesús Hernández
 public class List {
+    Child Cab;
     
+    List ( ) { Cab = null; }
+    
+    private boolean Empty ( ) { return Cab == null; } 
+    
+    private Child SearchCod ( int Cod ){
+        if ( Empty ( ) ) return null;
+        else {
+            Child A = Cab;
+            do{
+                if ( Cod == A.getId ( ) ) return A;
+                else A = A.Sig;
+            }while ( A != Cab );
+            return null;
+        }
+    }
+    
+    /*private Child Created ( String Grade, int Age, int Id, String Name,
+            String Gender ) {
+        
+    }*/
 }
